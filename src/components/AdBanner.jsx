de3c43893a3af550002f3d399e3449c2fd0af303
israel.html <israeleromon@gmail.com>
2026-06-22 21:06:19 +0100

    Update AdBanner with new 468x60 visible Adsterra script

--- src/components/AdBanner.jsx
+++ src/components/AdBanner.jsx
@@ -9,17 +9,30 @@ export default function AdBanner() {
       return;
     }
 
-    const script = document.createElement('script');
-    script.src = "https://pl29841604.effectivecpmnetwork.com/d5/76/c8/d576c838e07cf6e032598902579244d0.js";
-    script.async = true;
-    script.type = 'text/javascript';
+    const confScript = document.createElement('script');
+    confScript.type = 'text/javascript';
+    confScript.innerHTML = `
+      atOptions = {
+        'key' : '2da0ea1860bb5157c5ae4533357fd255',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+      };
+    `;
+    bannerRef.current.appendChild(confScript);
+
+    const invokeScript = document.createElement('script');
+    invokeScript.src = "https://bogavoidmemorize.com/2da0ea1860bb5157c5ae4533357fd255/invoke.js";
+    invokeScript.async = true;
+    invokeScript.type = 'text/javascript';
     
-    bannerRef.current.appendChild(script);
+    bannerRef.current.appendChild(invokeScript);
   }, []);
 
   return (
     <div className="flex justify-center items-center mt-6 w-full overflow-hidden">
-      <div ref={bannerRef} className="text-center w-full flex justify-center" />
+      <div ref={bannerRef} className="text-center w-full flex justify-center min-h-[60px]" />
     </div>
   );
 }
